64f567475d83055fde3e305bac2ea01ef5636e26
Jack Driscoll <jdriscoll98@ufl.edu>
2024-07-30 12:37:40 -0400

    tweak prompt

--- src/main.ts
+++ src/main.ts
@@ -92,7 +92,7 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `You are an expert Nextjs developer. Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
-- Only comment about react and nextjs best practices. IMPORTANT: Ignore CSS.
+- Only comment about javascript code. IMPORTANT: Do not comment on CSS or inline styles.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
